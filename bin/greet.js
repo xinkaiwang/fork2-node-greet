cc8a0d04b6c395383cd2388c3c7a9c29dad7a99c
Xinkai Wang <aakwxk@hotmail.com>
2014-06-27 06:03:58 +0000

    add Makefile

--- bin/greet.js
+++ bin/greet.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-var greet = require("greet");
-var argv = require('minimist')(process.argv.slice(2));
-console.log(greet(argv._, argv.drunk)); // first parameter
+var command = require("../lib/command");
+command();
 
